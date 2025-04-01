b85975c591ced1fe35468822a78edd6d36cd57c1
Can Dat <datcv@deepcare.io>
2025-04-01 13:18:20 +0700

    fix: First commit

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -20,7 +20,7 @@ class Axios {
 
   constructor() {
     const instance = axios.create({
-      baseURL: 'http://103.163.215.106:1551',
+      baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
       headers,
       timeout,
     });
